40968ca2525c0ac0d28ad081e0df62a6a0b38eeb
Alloh NKefor Rodrick <allohnkeforrodrick@gmail.com>
2025-11-26 13:39:21 +0100

    fixing issues related to keys

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "noble-ecart" });
+export const inngest = new Inngest({ 
+    id: "noble-ecart",
+    signingKey: process.env.INNGEST_SIGNING_KEY,
+ });
